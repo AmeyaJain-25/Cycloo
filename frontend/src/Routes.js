b1a2f7747a477063bef5eecc64d0ba837c832ec9
ShubhamSj07 <shubhamsj077@gmail.com>
2021-10-08 17:38:13 +0530

    [UI] Navbar added!

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./pages/loginpage/Login";
 import HomePage from "./pages/homepage/HomePage";
 import { app } from "./utils/firebaseConfig";
+import Navbar from "./components/Navbar/Navbar";
 
 const Routes = () => {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/login" component={Login} />
