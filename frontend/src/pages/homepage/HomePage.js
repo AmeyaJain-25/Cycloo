bc983fd0eac18bffd9f884c5c341ed31d55aea8e
Adarsh Shete <shete.adarsh@gmail.com>
2021-10-09 02:26:42 +0530

    filtering products code done

--- frontend/src/pages/homepage/HomePage.js
+++ frontend/src/pages/homepage/HomePage.js
@@ -1,16 +1,61 @@
 import { Col, Row } from "reactstrap";
 import Sidebar from "./Sidebar";
 import Navbar from "../../components/Navbar/Navbar";
+import ProductCard from "../../components/Card/ProductCard";
+import { getAllProducts } from "./helper/apiCalls";
+import { useEffect, useState } from "react";
+import "./homepage.scss";
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+  const [filteredPro, setFilteredPro] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchAllProducts = () => {
+    setLoading(true);
+    getAllProducts()
+      .then((res) => {
+        console.log("RES:", res);
+        setProducts(res);
+        setFilteredPro(res);
+      })
+      .catch((err) => {
+        console.log("err: ", err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+
   return (
-    <div>
+    <div className="homepage">
       <Navbar />
       <Row>
-        <Col md={4}>
-          <Sidebar />
+        <Col md={3}>
+          <Sidebar products={products} setFilteredPro={setFilteredPro} />
         </Col>
-        <Col md={8}></Col>
+        <Col md={9}>
+          <div className="products-container">
+            {!loading ? (
+              filteredPro.length ? (
+                filteredPro.map((productObj) => (
+                  <ProductCard
+                    key={productObj.productId}
+                    productObj={productObj}
+                  />
+                ))
+              ) : (
+                <p>No Products Found!</p>
+              )
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+        </Col>
       </Row>
     </div>
   );
