e1db8ee8bb716132e869a5f09600de4eb4d59f96
ShubhamSj07 <shubhamsj077@gmail.com>
2021-10-09 11:03:08 +0530

    Loader added, routes updated!

---
+++ frontend/src/components/Loader/Loader.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Loader.scss";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div class="sk-chase">
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
