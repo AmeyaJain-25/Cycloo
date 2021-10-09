4ec034d6365dbc067757de4e122dc1fc52d01463
AmeyaJain-25 <ameiya.jain@gmail.com>
2021-10-09 13:11:56 +0530

    Login Page Done

--- frontend/src/pages/loginpage/Login.js
+++ frontend/src/pages/loginpage/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
       "recaptcha-container",
       {
         size: "invisible",
-        callback: response => {
+        callback: (response) => {
           // reCAPTCHA solved, allow signInWithPhoneNumber.
           //   onSignInSubmit();
           console.log("CAPTCHA SOLVED");
@@ -38,7 +38,7 @@ const Login = () => {
     const phoneNumber = "+911122334455";
     const appVerifier = window.recaptchaVerifier;
     signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-      .then(confirmationResult => {
+      .then((confirmationResult) => {
         // SMS sent. Prompt user to type the code from the message, then sign the
         // user in with confirmationResult.confirm(code).
         window.confirmationResult = confirmationResult;
@@ -46,7 +46,7 @@ const Login = () => {
         setCodeSent(true);
         // ...
       })
-      .catch(error => {
+      .catch((error) => {
         // Error; SMS not sent
         console.log("ERROR: ", error);
         setCodeSent(false);
@@ -60,25 +60,25 @@ const Login = () => {
     setLoading(true);
     window.confirmationResult
       .confirm(code)
-      .then(result => {
+      .then((result) => {
         // User signed in successfully.
         const user = result.user;
         console.log("USER: ", user);
-        auth.currentUser.getIdToken(true).then(idToken => {
+        auth.currentUser.getIdToken(true).then((idToken) => {
           authenticateUser(idToken)
-            .then(res => {
+            .then((res) => {
               console.log("RES: ", res);
               history.push("/");
             })
-            .catch(err => {
+            .catch((err) => {
               console.log("ERR: ", err);
             })
-            .catch(err => {
+            .catch((err) => {
               console.log("ERR: ", err);
             });
         });
       })
-      .catch(error => {
+      .catch((error) => {
         // User couldn't sign in (bad verification code?)
         console.log("ERROR: ", error);
       })
@@ -89,13 +89,11 @@ const Login = () => {
 
   return (
     <div className="loginpage">
-      <Row>
-        <Col md={6}>
-          <div className="hero-section">
-            <img className="cycloo-logo" src={LOGO} alt="Cycloo" />
-            <p className="page-sub-title">When in doubt, pedal it out!</p>
-            <img src={BannerCycle} alt="banner_cycle" />
-          </div>
+      <Row className="loginPageRow">
+        <Col md={6} className="hero-section">
+          <img className="cycloo-logo" src={LOGO} alt="Cycloo" />
+          <p className="page-sub-title">When in doubt, pedal it out!</p>
+          <img src={BannerCycle} alt="banner_cycle" className="banner_cycle" />
         </Col>
         <Col md={6}>
           <Form className="login-form">
@@ -105,7 +103,7 @@ const Login = () => {
             <FormGroup>
               <Input
                 value={phoneNumber}
-                onChange={e => setPhoneNumber(e.target.value)}
+                onChange={(e) => setPhoneNumber(e.target.value)}
                 placeholder="Enter Phone Number"
                 className="login-input"
                 disabled={codeSent}
@@ -115,7 +113,7 @@ const Login = () => {
               <Input
                 value={code}
                 disabled={!codeSent}
-                onChange={e => setCode(e.target.value)}
+                onChange={(e) => setCode(e.target.value)}
                 placeholder="Enter OTP"
                 className="login-input"
               />
